8ae5daa34aa3e6c13742f61f3eb8df0c5fdc53d0
Dylan <76132079+dw-Apples-And-Bananas@users.noreply.github.com>
2023-09-23 19:21:59 +0100

    Create strhid.py

---
+++ src/strhid.py
@@ -0,0 +1,308 @@
+hid = {
+  # MDIFIERS
+  "control": 0x01,
+  "shift": 0x02,
+  "alt": 0x04,
+  "gui": 0x08,
+  "control+shift": 0x12,
+  "control+alt": 0x41,
+  "control+gui": 0x81,
+
+  # KEYS
+  "a": 0x04,
+  "b": 0x05,
+  "c": 0x06
+  "d": 0x07,
+  "e": 0x08,
+  "f": 0x09,
+  "g": 0x0a,
+  "h": 0x0b,
+  "i": 0x0c,
+  "j": 0x0d,
+  "k": 0x0e,
+  "l": 0x0f,
+  "m": 0x10,
+  "n": 0x11,
+  "o": 0x12,
+  "p": 0x13,
+  "q": 0x14,
+  "r": 0x15,
+  "s": 0x16,
+  "t": 0x17,
+  "u": 0x18,
+  "v": 0x19,
+  "w": 0x1a,
+  "x": 0x1b,
+  "y": 0x1c,
+  "z": 0x1d,
+
+  "1": 0x1e,
+  "2":
+  "3"
+  "4
+  "5
+  "6
+  "7
+  "8
+  "9
+  "0
+  
+  "enter"
+  "escape"
+  "backspace"
+  "tab"
+  "space"
+  "minus"
+  "equal"
+  "leftbrace"
+  "rightbrace"
+  "backslash"
+  "hash"
+  "semicolon"
+  "apostrophe"
+  "grave"
+  "comma"
+  "dot"
+  "slash"
+  "capslock"
+
+  "f1"
+  "f2"
+  "f3"
+  "f4"
+  "f5"
+  "f6"
+  "f7"
+  "f8"
+  "f9"
+  "f10"
+  "f11"
+  "f12"
+  
+  "sysrq"
+  "scrolllock"
+  "pause"
+  "insert"
+  "home"
+  "pageup"
+  "delete"
+  "end"
+  "pagedown"
+  "right"
+  "left"
+  "down"
+  "up"
+
+  "numlock"
+  "kpslash"
+  "kpasterisk"
+  "kpminus"
+  "kpplus"
+  "kpenter"
+  "kp1"
+  "kp2"
+  "kp3"
+  "kp4"
+  "kp5"
+  "kp6"
+  "kp7"
+  "kp8"
+  "kp9"
+  "kp0"
+  "kpdot"
+  
+  "102ND"
+  "compose"
+  "power"
+  "kpequal"
+  
+  "f13"
+  "f14"
+  "f15"
+  "f16"
+  "f17"
+  "f19"
+  "f20"
+  "f21"
+  "f22"
+  "f23"
+  "f24"
+  
+  "open"
+  "help"
+  "props"
+  "front"
+  "stop"
+  "again"
+  "undo"
+  "cut"
+  "copy"
+  "paste"
+  "find"
+  
+  "kpcomma"
+  "ro"
+  "katakanahirigana"
+  "yen"
+  "henkan"
+  "muhenkan"
+  "kpjpcomma"
+  "hanguel"
+  "hanja"
+  "katakana"
+  "hirigana"
+  "zenkakuhankaku"
+  
+  "kpleftparen"
+  "kprightparen"
+  
+  "playpause"
+  "stopcd"
+  "previoussong"
+  "nextsong"
+  "ejectcd"
+  "volumeup"
+  "volumedown"
+  "mute"
+  "www"
+  "back"
+  "forward"
+  "stop"
+  "scrollup"
+  "scrolldown"
+  "edit"
+  "sleep"
+  "coffee"
+  "refresh"
+  "calc"
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  "
+  
+
+
+
+  
